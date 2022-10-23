139248742d5861a493ef38b068d18db463bc7650
Muskan-Lama <114075496+Muskan-Lama@users.noreply.github.com>
2022-10-24 01:30:29 +0530

    Update Btech.java

--- src/main/java/com/TTN/springboot/question3/Btech.java
+++ src/main/java/com/TTN/springboot/question3/Btech.java
@@ -3,7 +3,7 @@ package com.TTN.springboot.question3;
 import org.springframework.stereotype.Component;
 
 @Component
-public class bTech implements computerScience {
+public class Btech implements ComputerScience {
 
 
     @Override
